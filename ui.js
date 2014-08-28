09207cda2a6312dbc87d50b933023872ae01fa28
lily <hotaru@thinkindifferent.net>
2014-08-28 12:46:30 -0400

    clean up crypto code

--- ui.js
+++ ui.js
@@ -45,24 +45,28 @@ function verify_signature(id, n, data)
     { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-512' }},
     true, ['verify']).then(
     function(key)
-    { crypto.subtle.verify(
+    { return crypto.subtle.verify(
         { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-512' }},
-          key, signature_data, comment_data).then(
-          function(result)
-          { if(result)
-            { $('#' + id + '_' + (n + 1)).addClass('valid');
-              var k = data.key.n;
-              var length = k.length;
-              var key_data = new Uint8Array(length);
-              for(var j = 0; j < length; ++j) key_data[j] = k.charCodeAt(j);
-              crypto.subtle.digest({ name: 'SHA-1' }, key_data).then(
-                function(digest)
-                { var digest_data = new Uint8Array(digest);
-                  var a = new Array();
-                  for(var j = 0; j < 20; ++j) a[j] = digest_data[j]; 
-                  $('#' + id + '_' + (n + 1)).attr('title', btoa(a)); }); }
-            else
-              $('#' + id + '_' + (n + 1)).addClass('invalid'); }); }); }
+          key, signature_data, comment_data); },
+    console.error.bind('Unable to import public key.')).then(
+    function(result)
+    { if(result)
+      { $('#' + id + '_' + (n + 1)).addClass('valid');
+        var k = data.key.n;
+        var length = k.length;
+        var key_data = new Uint8Array(length);
+        for(var j = 0; j < length; ++j) key_data[j] = k.charCodeAt(j);
+        return crypto.subtle.digest({ name: 'SHA-1' }, key_data); }
+      else
+        $('#' + id + '_' + (n + 1)).addClass('invalid'); },
+    console.error.bind('Unable to verify signature.')).then(
+    function(digest)
+    { if(digest)
+      { var digest_data = new Uint8Array(digest);
+        var a = new Array();
+        for(var j = 0; j < 20; ++j) a[j] = digest_data[j]; 
+        $('#' + id + '_' + (n + 1)).attr('title', btoa(a)); }},
+    console.error.bind('Unable to compute hash.')); }
 
 function reply_form(id)
 { var container = $('<div id="reply_form">');
@@ -83,9 +87,11 @@ function generate_key()
      hash: { name: 'SHA-512' } },
    true, ['sign', 'verify']).then(
    function(key)
-   { crypto.subtle.exportKey('jwk', key.privateKey).then(
-      function(exported_key)
-      { $('#key').val(JSON.stringify(exported_key)); }); }); }
+   { return crypto.subtle.exportKey('jwk', key.privateKey); },
+   console.error.bind(console, 'Unable to generate key.')).then(
+   function(exported_key)
+   { $('#key').val(JSON.stringify(exported_key)); },
+   console.error.bind('Unable to export key.')); }
 
 function submit_form()
 { var title = $('#title').val();
@@ -108,23 +114,28 @@ function submit_form()
         delete key_data.q;
         delete key_data.qi;
         key_data.key_ops = ['verify']
-        crypto.subtle.importKey('jwk', key_data,
+        return crypto.subtle.sign(
+          { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-512' }},
+          private_key, comment_data); },
+      console.error.bind('Unable to import private key.')).then(
+      function(signature_buffer)
+      { var signature_data = new Uint8Array(signature_buffer);
+        var signature = new Array(256);
+        for(var i = 0; i < 256; ++i) signature[i] = signature_data[i];
+        return signature; },
+      console.error.bind('Unable to sign.')).then(
+      function(signature)
+      { crypto.subtle.importKey('jwk', key_data,
           { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-512' }},
           true, ['verify']).then(
-          function(public_key)
-          { crypto.subtle.sign(
-              { name: 'RSASSA-PKCS1-v1_5', hash: { name: 'SHA-512' }},
-              private_key, comment_data).then(
-              function(signature_buffer)
-              { var signature_data = new Uint8Array(signature_buffer);
-                var signature = new Array(256);
-                for(var i = 0; i < 256; ++i) signature[i] = signature_data[i];
-                $.post('post',
-                       { 'title': title, 'thread': thread, 'comment': comment,
-                         'key': JSON.stringify(key_data),
-                         'signature': JSON.stringify(signature) },
-                       function() { location.reload(); }).fail(
-                       function() { alert('Post failed. Please check your input and try again.'); }); }); }); }); }
+        function(public_key)
+        { $.post('post',
+            { 'title': title, 'thread': thread, 'comment': comment,
+              'key': JSON.stringify(key_data),
+              'signature': JSON.stringify(signature) },
+            function() { location.reload(); }).fail(
+            function() { alert('Post failed. Please check your input and try again.'); }); },
+        console.error.bind('Unable to import public key.')); }); }
   else $.post('post', { 'title': title, 'thread': thread, 'comment': comment },
          function() { location.reload(); }).fail(
          function() { alert('Post failed. Please check your input and try again.'); }); }
